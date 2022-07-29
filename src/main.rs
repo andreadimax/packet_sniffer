9d339595810b5ec36fe6afb79e335ed065318c8a
andreadimax <andreacivitas@hotmail.it>
2022-07-29 18:47:01 +0200

    print devices list

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
+use pcap::Device;
+
 fn main() {
-    // get the default Device
-    let device = pcap::Device::lookup()
-        .expect("device lookup failed");
-    println!("Using device {}", device.name);
+    
+    //get devices list
+    let devices_list = Device::list().unwrap();
 
-    // Setup Capture
-    let mut cap = pcap::Capture::from_device(device)
-        .unwrap()
-        .immediate_mode(true)
-        .open()
-        .unwrap();
+    //printing devices list
+    println!("Available devices:");
 
-    // get a packet and print its bytes
-    println!("{:?}", cap.next());
+    for device in &devices_list {
+        match &device.desc {
+            Some(description) => {
+                println!("{} - {}", &device.name, String::from(description));
+            },
+            None => {
+                println!("{} - No description available", &device.name);
+            }
+        }
+    }
+
+    //user input
+    let mut device_to_monitor = String::new();
+
+    loop {
+        println!("\nType the device you want to monitor or quit to exit:");
+        device_to_monitor.clear();
+        std::io::stdin().read_line(& mut device_to_monitor).unwrap();
+
+        device_to_monitor = device_to_monitor.replace("\n", "");
+    
+        match device_to_monitor.as_str() {
+            "quit" => {
+                println!("Quitting...");
+                return
+            },
+            _ => {
+                for device in &devices_list {
+                    if device.name == device_to_monitor {
+                        unimplemented!("still some months to work...");
+                    }
+                }
+            }
+        }
+    }
+
+
 }
