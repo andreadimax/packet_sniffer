47a8962ec10e56510cfed98905a744ca5aec739f
andreadimax <andreacivitas@hotmail.it>
2022-08-19 13:06:25 +0200

    add tests

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+export RUSTFLAGS=
+export RUSTC_BOOTSTRAP=0
+cargo build
+cargo test --
+rm ./target/debug/lcov.info
+rm -rf ./target/debug/result
 grcov . -s . --ignore "$HOME/.cargo/registry/*" --binary-path ./target/debug/packet_sniffer -t lcov --branch --ignore-not-existing -o ./target/debug/lcov.info
 genhtml -o ./target/debug/result --show-details --highlight --ignore-errors source --legend ./target/debug/lcov.info
